1671e7674921d2c99319a129e25c757568820220
SFIV <shamsfarhan@iut-dhaka.edu>
2023-09-19 02:57:59 +0600

    Added more Icons to Main Menu and Added Today's Date

--- src/main/java/com/example/blood/MainMenuController.java
+++ src/main/java/com/example/blood/MainMenuController.java
@@ -2,10 +2,30 @@ package com.example.blood;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.awt.*;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
 
 import java.io.IOException;
+import java.util.ResourceBundle;
 
-public class MainMenuController {
+public class MainMenuController implements Initializable {
+    SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+    Date date = new Date();
+    String temp = formatter.format(date);
+    @FXML
+    Label Dt;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+        Dt.setText("Today's Date: " + temp);
+    }
     @FXML protected void onLogoutBC(ActionEvent event) throws IOException {
         switchMenu.MenuSwitch(event, switchMenu.loginMenu, "Login");
     }
